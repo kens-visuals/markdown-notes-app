f3684ccb3da6ed7a10545a332e9b4960f5f6d4f6
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-08-04 16:01:18 -0700

    add Head to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Head from 'next/head';
 
 // Components
 import Sidebar from '../components/Sidebar';
@@ -9,6 +10,41 @@ export default function Home() {
 
   return (
     <div>
+      <Head>
+        <title>Markdown App</title>
+        <meta
+          name="description"
+          content="Markdown editor built with NextJS and Firebase Firestore. Coded by Kens-Visuals"
+        />
+        <meta property="og:title" content="Markdown App" />
+        <meta name="author" content="Kens-Visuals" />
+        <meta
+          property="og:url"
+          content="https://markdown-notes-app-delta.vercel.app/"
+        />
+        <meta property="og:image" content="https://mugshotbot.com/m/P34RyMqb" />
+        <meta property="twitter:card" content="summary_large_image" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <div className="flex overflow-auto selection:bg-orange-primary selection:text-white">
         {isSidebarOpen && <Sidebar />}
 
