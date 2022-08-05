6d1f8bbea608434edb6ef97557402cdb36968a15
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-08-04 18:45:03 -0700

    fix a11y issues

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -22,10 +22,7 @@ export default function ThemeToggler() {
   };
 
   return (
-    <label
-      htmlFor="theme-toggle"
-      className="relative inline-flex cursor-pointer items-center gap-4"
-    >
+    <div className="relative inline-flex cursor-pointer items-center gap-4">
       <Image
         src={moonIcon}
         alt="dark mode"
@@ -37,14 +34,16 @@ export default function ThemeToggler() {
         }`}
       />
 
-      <input
-        type="checkbox"
-        value={checked}
-        checked={checked}
-        onChange={handleCheckedChange}
-        id="theme-toggle"
-        className="peer sr-only"
-      />
+      <label htmlFor="theme-toggle">
+        <input
+          type="checkbox"
+          value={checked}
+          checked={checked}
+          onChange={handleCheckedChange}
+          id="theme-toggle"
+          className="peer sr-only"
+        />
+      </label>
       <div className="peer h-6 w-11 rounded-full bg-secondary-600 after:absolute after:top-[2px] after:left-[35px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all peer-checked:bg-secondary-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-orange-primary dark:border-gray-600 dark:bg-secondary-600 " />
 
       <Image
@@ -57,6 +56,6 @@ export default function ThemeToggler() {
           theme === 'light' ? 'opacity-100' : 'opacity-50'
         }`}
       />
-    </label>
+    </div>
   );
 }
